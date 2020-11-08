6938fefc2bea9b22fb1b7100319746075e3fa355
Sergij18Klym <derstig@ukr.net>
2020-11-08 15:29:36 +0100

    yuitit

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ const myPeer = new Peer(undefined, {
 const myPeer = new Peer(undefined, {
   secure: true, 
   host: 'videowebdev.herokuapp.com',
-  port: '3000',
+  port: '3001',
   path: '/peerjs'
 })
 
